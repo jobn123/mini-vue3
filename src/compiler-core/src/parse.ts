3f0bec46acb204a5e58738db95af0c0c9958492d
jobn123 <jobn_joy@163.com>
2022-05-03 15:35:23 +0800

    feat: add parse Text

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -27,9 +27,30 @@ function parseChildren (context) {
     }
   }
 
+  if (!node) {
+    node = parseText(context)
+  }
+
   nodes.push(node)
 
   return nodes
+}
+
+function parseText (context: any): any {
+  const content = parseTextData(context, context.source.length)
+
+  return {
+    type: NodeTypes.TEXT,
+    content
+  }
+}
+
+function parseTextData (context: any, length): any {
+  const content = context.source.slice(0, length)
+
+  advanceBy(context, content.length)
+
+  return content
 }
 
 function parseInterpolation (context) {
@@ -42,7 +63,7 @@ function parseInterpolation (context) {
 
   const rawContentLength = closeIndex - openDelimiter.length
 
-  const rawContent = context.source.slice(0, rawContentLength)
+  const rawContent = parseTextData(context, rawContentLength)
   const content = rawContent.trim()
 
   advanceBy(context, rawContentLength + closeDelimiter.length)
@@ -97,3 +118,4 @@ function parseTag (context: any, type: TagType) {
     tag,
   }
 }
+
